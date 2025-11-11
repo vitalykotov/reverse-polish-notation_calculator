59adcc95d27aa3abbfe3f002bc0094df86ad6c45
vitalykotov <vitalik.mishl.kotov@mail.ru>
2025-11-11 10:44:32 +0300

    True test

--- src/test/java/ru/ac/uniyar/rpncalculator/RPNEvaluatorTest.java
+++ src/test/java/ru/ac/uniyar/rpncalculator/RPNEvaluatorTest.java
@@ -139,7 +139,7 @@ public class RPNEvaluatorTest {
     void testDemonstrateFailure() {
         String[] tokens = {"2", "+", "3"};
         List<String> result = RPNEvaluator.trans(tokens, pri);
-        assertEquals(List.of("2", "3", "+","ERROR"), result,
+        assertEquals(List.of("2", "3", "+"), result,
                 "Этот тест должен упасть - в ожидании лишний элемент 'ERROR'");
     }
 }
